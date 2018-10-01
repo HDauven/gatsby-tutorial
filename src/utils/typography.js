d984b8b7ff56f1c180bcc36667dddc80f6a8bb04
Hein Dauven <heindauven@gmail.com>
2018-10-01 15:39:28 +0200

    Add Typography configuration

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography";
+
+// import bootstrapTheme from "typography-theme-bootstrap"
+import fairyGateTheme from "typography-theme-fairy-gates";
+
+const typography = new Typography(fairyGateTheme);
+
+export default typography;
