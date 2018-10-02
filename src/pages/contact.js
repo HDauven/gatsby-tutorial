4cf8d5bb2f46c3427d2cc91ee0395c2702ab9a47
Hein Dauven <heindauven@gmail.com>
2018-10-02 17:33:07 +0200

    Add a contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+import Header from "../components/header";
+import Layout from "../components/layout";
+
+const ContactPage = () => (
+  <Layout>
+    <Header headerText="Contact" />
+    <p>Drop us a message!</p>
+  </Layout>
+);
+
+export default ContactPage;
